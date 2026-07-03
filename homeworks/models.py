dcd6a0d1b9d466453a531384b265e730dd525eb4
SOFIA RODRIGUEZ <sofirodriguez353@gmail.com>
2026-07-02 21:20:38 -0400

    models are done

--- homeworks/models.py
+++ homeworks/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# MODELO 1: PARA LOS PROYECTOS
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateField()  
+    
+    # El creador es el usuario que inició el proyecto, en otras palabras, el autor del proyecto. Esto permite que cada proyecto esté asociado a un usuario específico.
+    creador = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+
+
+# MODELO 2: PARA LAS TAREAS DE CADA PROYECTO
+class Tarea(models.Model):
+    # Opciones de estado para las tareas
+    ESTADOS = [
+        ('PENDIENTE', 'Pendiente'),
+        ('PROCESO', 'En Proceso'),
+        ('FINALIZADO', 'Finalizado'),
+    ]
+
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_limite = models.DateField()
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='PENDIENTE')
+    
+    # Vinculamos la tarea a un proyecto específico
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
